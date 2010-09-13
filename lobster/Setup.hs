2df12dcd7a884bd7f587d1bcb9ed910d44e01a94
Rogan Creswick <creswick@galois.com>
2010-09-13 11:52:49 -0700

    added clean pre-hook

--- lobster/Setup.hs
+++ lobster/Setup.hs
@@ -2,7 +2,8 @@ import Distribution.Simple
 import Distribution.Simple.Program.Types
 import Distribution.Simple.Setup
 import Distribution.Simple.UserHooks
-import Distribution.Simple.Utils ( rawSystemExit, warn, debug, findProgramVersion )
+import Distribution.Simple.Utils ( rawSystemExit, warn, debug
+                                 , findProgramVersion, notice )
 import Distribution.Verbosity ( Verbosity )
 
 import System.Directory ( getCurrentDirectory, setCurrentDirectory,
@@ -12,7 +13,8 @@ import System.Cmd       ( rawSystem )
 
 main = do
   defaultMainWithHooks simpleUserHooks
-       { preBuild = \a b -> bnfc a b >> preBuild simpleUserHooks a b
+       { preBuild = \a b -> generateBnfc a b >> preBuild simpleUserHooks a b
+       , preClean = \a b -> cleanBnfc a b >> preClean simpleUserHooks a b
        , hookedPrograms = [bnfcProgram] }
 
 bnfcProgram :: Program
@@ -32,23 +34,26 @@ bnfcFiles = [ "src/Lobster/Abs.hs"
             , "src/Lobster/Test.hs"
             ]
 
--- translation of this make file rule:
--- $(BNFC_FILES): src/Lobster/Lobster.cf
--- 	rm -f $(BNFC_FILES)
--- 	cd src/Lobster && $(BNFC) -d Lobster.cf
--- 	mv src/Lobster/Lobster/*.* src/Lobster
--- 	rmdir src/Lobster/Lobster
+cleanBnfc :: Args -> CleanFlags -> IO ()
+cleanBnfc _ flags = do
+  let verbosity = (fromFlag $ cleanVerbosity flags)
+  removeBnfcFiles verbosity
 
-bnfc :: Args -> BuildFlags -> IO ()
-bnfc _ flags = do
+generateBnfc :: Args -> BuildFlags -> IO ()
+generateBnfc _ flags = do
   let verbosity = (fromFlag $ buildVerbosity flags)
-  warn verbosity "Running bnfc build"
-  mapM_ (removeFileIfExists verbosity) bnfcFiles
+  notice verbosity "Running bnfc build"
+  removeBnfcFiles verbosity
   -- cd src && bnfc -d Lobster/Lobster.cf
   orig <- getCurrentDirectory
   setCurrentDirectory "src"
   rawSystemExit verbosity "bnfc" ["-d", "Lobster/Lobster.cf"]
   setCurrentDirectory orig
+
+removeBnfcFiles :: Verbosity -> IO ()
+removeBnfcFiles verbosity = do
+  notice verbosity "Cleaning bnfc files"
+  mapM_ (removeFileIfExists verbosity) bnfcFiles
 
 removeFileIfExists :: Verbosity -> FilePath -> IO ()
 removeFileIfExists v file = removeFile file `catch`
