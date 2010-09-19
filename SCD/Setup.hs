d5ebba4d9b31112613f3604242748ce6f607fcf3
Rogan Creswick <creswick@galois.com>
2010-09-18 19:17:27 -0700

    added checkpolicy build to the cabal build process

--- SCD/Setup.hs
+++ SCD/Setup.hs
@@ -1,2 +1,41 @@
+import System.Directory ( getCurrentDirectory, setCurrentDirectory
+                        , getDirectoryContents, copyFile, createDirectoryIfMissing)
 import Distribution.Simple
-main = defaultMain
+import Distribution.Verbosity
+import Distribution.Simple.Setup
+import Distribution.Simple.Utils (rawSystemExit, rawSystemStdout, info, warn, debug)
+import Distribution.PackageDescription (PackageDescription(..))
+import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(..), InstallDirs(..), absoluteInstallDirs)
+
+main = defaultMainWithHooks simpleUserHooks
+    { preConf = \a b -> make a b >> preConf simpleUserHooks a b
+    , preClean = \a b -> makeClean a b >> preClean simpleUserHooks a b
+    }
+
+-- | The location of the native source, relative to the project root:
+nativeCodePath :: FilePath
+nativeCodePath = "checkpolicy"
+
+nativeBuildDir :: FilePath -> FilePath
+nativeBuildDir cabalDir = cabalDir++"/dist/"++nativeCodePath
+
+make :: Args -> ConfigFlags -> IO ()
+make _ flags = do
+  let verbosity = (fromFlag $ configVerbosity flags)
+  cabalDir <- getCurrentDirectory
+  setCurrentDirectory nativeCodePath
+  rawSystemLog verbosity "make" []
+  setCurrentDirectory cabalDir
+
+makeClean :: Args -> CleanFlags -> IO ()
+makeClean _ flags = do
+  let verbosity = (fromFlag $ cleanVerbosity flags)
+  origDir <- getCurrentDirectory
+  setCurrentDirectory nativeCodePath
+  rawSystemLog verbosity "make" ["clean"]
+  setCurrentDirectory origDir
+
+
+-- | Execute a system command, logging the output at `info` level.
+rawSystemLog :: Verbosity -> String -> [String] -> IO ()
+rawSystemLog v cmd args = rawSystemStdout v cmd args >>= info v
