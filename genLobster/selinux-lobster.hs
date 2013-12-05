74d67334b61d3956592db510c1846b1a0361e3a3
Brian Huffman <huffman@galois.com>
2013-12-05 12:53:43 -0800

    Convert selinux-lobster to use Lobster AST/pretty-printer from SCD.Lobster.Gen (no more extra spaces around periods in printed output)

--- genLobster/selinux-lobster.hs
+++ genLobster/selinux-lobster.hs
@@ -25,8 +25,8 @@ import Data.Set (Set)
 
 import Data.NonEmptyList (NonEmptyList)
 
-import qualified Lobster.Abs as L
-import qualified Lobster.Print as P
+import qualified SCD.Lobster.Gen.CoreSyn as L
+import SCD.Lobster.Gen.CoreSyn.Output (showLobster)
 
 import SCD.SELinux.Syntax
 import SCD.SELinux.Parser (parsePolicy)
@@ -111,8 +111,8 @@ processTeRbac te =
     -- Optional AvRuleBlock (Maybe AvRuleBlock)
     _ -> return ()
 
-processPolicy :: Policy -> L.Policy
-processPolicy policy = L.Policy (preDecls ++ classDecls ++ classDecls' ++ domainDecls ++ connectionDecls)
+processPolicy :: Policy -> [L.Decl]
+processPolicy policy = preDecls ++ classDecls ++ classDecls' ++ domainDecls ++ connectionDecls
   where
     finalSt :: St
     finalSt = execState (mapM_ processTeRbac (teRbacs policy)) initSt
@@ -121,65 +121,65 @@ processPolicy policy = L.Policy (preDecls ++ classDecls ++ classDecls' ++ domain
       Set.unions $
         [ fromMaybe Set.empty (Map.lookup x (object_classes finalSt))
         | x <- Set.toList (actors finalSt) ]
-    toPortId :: PermissionId -> L.PortId
-    toPortId = L.PortId . L.LIdent . idString
-    toClassId :: ClassId -> L.ClassId
-    toClassId = L.ClassId . L.UIdent . capitalize . idString
-    toIdentifier :: TypeOrAttributeId -> L.Identifier
-    toIdentifier = L.Identifier . L.LIdent . lowercase . idString
-    mkPortDecl :: L.PortId -> L.Statement
-    mkPortDecl port = L.PortDeclaration port L.EmptyPDT L.EmptyPDC
-    mkDomDecl :: L.Identifier -> L.ClassId -> L.Statement
-    mkDomDecl dom cls = L.DomainDeclaration dom (L.ClassInstantiation cls [])
+    toPortId :: PermissionId -> L.Name
+    toPortId = L.Name . idString
+    toClassId :: ClassId -> L.Name
+    toClassId = L.Name . capitalize . idString
+    toIdentifier :: TypeOrAttributeId -> L.Name
+    toIdentifier = L.Name . lowercase . idString
+    mkPortDecl :: L.Name -> L.Decl
+    mkPortDecl port = L.newPort port
+    mkDomDecl :: L.Name -> L.Name -> L.Decl
+    mkDomDecl dom cls = L.Domain dom cls []
     commonMap :: Map CommonId [PermissionId]
     commonMap = Map.fromList [ (i, toList ps) | CommonPerm i ps <- commonPerms policy ]
-    activePort :: L.PortId
-    activePort = L.PortId (L.LIdent "active")
-    subjectClass :: L.ClassId
-    subjectClass = (L.ClassId (L.UIdent "SUBJECT"))
-    unknownClass :: L.ClassId
-    unknownClass = (L.ClassId (L.UIdent "UNKNOWN"))
-    preDecls :: [L.Statement]
+    activePort :: L.Name
+    activePort = L.Name "active"
+    subjectClass :: L.Name
+    subjectClass = L.Name "SUBJECT"
+    unknownClass :: L.Name
+    unknownClass = L.Name "UNKNOWN"
+    preDecls :: [L.Decl]
     preDecls =
-        [ L.ClassDeclaration subjectClass [] [mkPortDecl activePort]
-        , L.ClassDeclaration unknownClass [] [] ]
-    classDecls :: [L.Statement]
+        [ L.Class subjectClass [] [mkPortDecl activePort]
+        , L.Class unknownClass [] [] ]
+    classDecls :: [L.Decl]
     classDecls = map classDecl (toList (avPerms policy))
-    classDecl :: AvPerm -> L.Statement
+    classDecl :: AvPerm -> L.Decl
     classDecl (AvPermClass classId e) =
-      L.ClassDeclaration (toClassId classId) [] (active ++ stmts)
+      L.Class (toClassId classId) [] (active ++ stmts)
       where
         isActive :: Bool
         isActive = Set.member classId activeClasses
-        active :: [L.Statement]
+        active :: [L.Decl]
         active = if isActive then [mkPortDecl activePort] else []
         perms :: [PermissionId]
         perms = case e of
                   Left ps -> toList ps
                   Right (commonId, ps) -> fromMaybe [] (Map.lookup commonId commonMap) ++ ps
-        stmts :: [L.Statement]
+        stmts :: [L.Decl]
         stmts = map (mkPortDecl . toPortId) perms
-    classDecls' :: [L.Statement]
+    classDecls' :: [L.Decl]
     classDecls' = concatMap classDecl' (Set.toList (all_types finalSt))
-    classDecl' :: TypeOrAttributeId -> [L.Statement]
+    classDecl' :: TypeOrAttributeId -> [L.Decl]
     classDecl' typeId =
       case fmap Set.toList $ Map.lookup typeId (object_classes finalSt) of
         Just [c] -> []
-        _ -> [L.ClassDeclaration classId [] (active ++ stmts)]
+        _ -> [L.Class classId [] (active ++ stmts)]
           where
-            classId :: L.ClassId
-            classId = L.ClassId . L.UIdent . ("TE_" ++) . idString $ typeId
+            classId :: L.Name
+            classId = L.Name . ("TE_" ++) . idString $ typeId
             isActive :: Bool
             isActive = Set.member typeId (actors finalSt)
-            active :: [L.Statement]
+            active :: [L.Decl]
             active = if isActive then [mkPortDecl activePort] else []
             perms :: [PermissionId]
             perms = Set.toList $ fromMaybe Set.empty $ Map.lookup typeId (object_perms finalSt)
-            stmts :: [L.Statement]
+            stmts :: [L.Decl]
             stmts = map (mkPortDecl . toPortId) perms
-    domainDecls :: [L.Statement]
+    domainDecls :: [L.Decl]
     domainDecls = map domainDecl (Set.toList (all_types finalSt))
-    domainDecl :: TypeOrAttributeId -> L.Statement
+    domainDecl :: TypeOrAttributeId -> L.Decl
     domainDecl typeId = mkDomDecl (toIdentifier typeId) classId
       where
         classId =
@@ -188,27 +188,21 @@ processPolicy policy = L.Policy (preDecls ++ classDecls ++ classDecls' ++ domain
                     | otherwise                          -> unknownClass
             Just s -> case Set.toList s of
                         [c] -> toClassId c
-                        _ -> L.ClassId . L.UIdent . ("TE_" ++) . idString $ typeId
-    connectionDecls :: [L.Statement]
+                        _ -> L.Name . ("TE_" ++) . idString $ typeId
+    connectionDecls :: [L.Decl]
     connectionDecls = map connectionDecl (reverse (allow_rules finalSt))
-    connectionDecl :: (TypeOrAttributeId, TypeOrAttributeId, PermissionId) -> L.Statement
+    connectionDecl :: (TypeOrAttributeId, TypeOrAttributeId, PermissionId) -> L.Decl
     connectionDecl (subject, object, perm) =
-        L.PortConnection
-          [toExpression subject activePort]
-          L.NeutralConnection
-          [toExpression object (toPortId perm)]
-    toExpression :: TypeOrAttributeId -> L.PortId -> L.Expression
-    toExpression typeId (L.PortId lident) = L.QualNameExpression (L.Qual (L.UnQual domName) portName)
-      where
-        domName = L.Ident (toIdentifier typeId)
-        portName = L.Ident (L.Identifier lident)
+        L.neutral
+          (L.domPort (toIdentifier subject) activePort)
+          (L.domPort (toIdentifier object) (toPortId perm))
 
 processPolicyFile :: Prelude.FilePath -> IO String
 processPolicyFile path = do
   s <- readFile path
   case parsePolicy path s of
     Left err -> return err
-    Right pol -> return $ P.printTree $ processPolicy pol
+    Right pol -> return $ showLobster $ processPolicy pol
 
 capitalize :: String -> String
 capitalize "" = ""
@@ -242,4 +236,4 @@ main = do
   s <- readFile filename
   case parsePolicy filename s of
     Left err -> die err
-    Right pol -> putStrLn $ P.printTree $ processPolicy pol
+    Right pol -> putStrLn $ showLobster $ processPolicy pol
